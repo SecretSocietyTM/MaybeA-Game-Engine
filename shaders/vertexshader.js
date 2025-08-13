65d6d178282578eb7aa77cb4da83aa17ffa0afc7
Dani J <djordan020403@gmail.com>
2025-08-13 15:36:29 -0700

    log: init cube
    A cube has been rendered. This commit contains the code for drawing a
    cube onto the canvas. The cube is with a function createCube. Currently
    only one cube can be drawn at a time since the EBO directly uses the
    faces of the cube to render said cube, introducing more cubes would
    cause an overlap of face indexes and they will reference the incorrect
    vertex within the VBO.
    
    A likely solution would be to provide a VBO and EBO per "mesh" or cube
    in this case, and iterating through each EBO in the while loop, but that may not be the most efficient way of doing things.
    
    Next: Camera Movement!

---
+++ shaders/vertexshader.js
@@ -0,0 +1,15 @@
+export default `
+attribute vec4 a_pos;
+attribute vec4 a_clr;
+
+varying vec4 v_clr;
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_proj;
+
+void main() {
+    gl_Position = u_proj * u_view * u_model * a_pos;
+    v_clr = a_clr;
+}
+`
