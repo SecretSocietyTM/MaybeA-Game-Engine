df377e934ca73f62f28bec1f9802bbadab8eb7d0
Dani J <djordan020403@gmail.com>
2025-08-16 22:34:34 -0700

    log: did basically nothing

--- main.js
+++ main.js
@@ -1,64 +1,24 @@
 const glm = glMatrix; // shorten math library name,
 const vec3 = glm.vec3;
+const vec4 = glm.vec4;
 const mat4 = glm.mat4;
-import { createCubeMultiColored, createPosClrInterleavedVao } from "./util/entity.js";
+import { Entity, colors, createCubeMultiColored, createPosClrInterleavedVao } from "./util/entity.js";
 import { createProgram, createStaticIndexBuffer, createStaticVertexBuffer, setupRender, getContext, showError } from "./util/gl_utils.js";
 import vsSrc from "../shaders/vertexshader.js";
 import fsSrc from "../shaders/fragmentshader.js";
 
-class Entity {
-  constructor(pos, scale, r_axis, r_angle, vao, num_indices) {
-/*     this.pos = pos;
-    this.scale = scale;
-    this.r_axis = r_axis,
-    this.r_angle = r_angle; */
-
-    this.vao = vao;
-    this.num_indices = num_indices;
-
-    // build model matrix
-    this.mat_model = mat4.create();
-    mat4.translate(this.mat_model, this.mat_model, pos);
-    mat4.rotate(this.mat_model, this.mat_model, glm.glMatrix.toRadian(r_angle), r_axis);
-    mat4.scale(this.mat_model, this.mat_model, [scale, scale, scale]);
-  }
-
-  draw(gl, model_uniform) {
-    gl.uniformMatrix4fv(model_uniform, false, this.mat_model);
-    gl.bindVertexArray(this.vao);
-    gl.drawElements(gl.TRIANGLES, this.num_indices, gl.UNSIGNED_SHORT, 0);
-    gl.bindVertexArray(null);
-  }
-}
-
 function main() {
     const canvas = document.getElementById("canvas");
-    if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
-        showError('Could not get Canvas reference');
-        return;
-    }
+
 
     const gl = getContext(canvas);
 
-    const colors = [
-        [0.2, 0.2, 0.2], [0.8, 0.2, 0.2],
-        [0.2, 0.8, 0.2], [0.2, 0.2, 0.8],
-        [0.2, 0.8, 0.8], [0.8, 0.2, 0.8],
-        [0.8, 0.8, 0.2], [0.8, 0.8, 0.8]]
-    const cube = createCubeMultiColored(colors);
+    const program = createProgram(gl, vsSrc, fsSrc);
 
+    const cube = createCubeMultiColored(colors);
     const cube_vertices = createStaticVertexBuffer(gl, cube.vertices);
     const cube_indices = createStaticIndexBuffer(gl, cube.indices);
 
-    if (!cube_vertices || !cube_indices) {
-        showError(`Failed to create entity: cube: (v=${!!cube_vertices} i=${!!cube_indices})`);
-    }
-
-    const program = createProgram(gl, vsSrc, fsSrc);
-    if (!program) {
-        showError("Failed to compile WebGL program");
-        return;
-    }
 
     const pos_attrib = gl.getAttribLocation(program, "a_pos");
     const clr_attrib = gl.getAttribLocation(program, "a_clr");
@@ -69,9 +29,9 @@ function main() {
 
     if (pos_attrib < 0 || clr_attrib < 0 || 
         !model_uniform || !view_uniform || !proj_uniform) {
-        showError(`Failed to get attribs/uniforms: ` +
-            `pos=${pos_attrib}, color=${clr_attrib} ` +
-            `u_model=${!!model_uniform} u_view=${!!view_uniform} u_proj=${!!proj_uniform}`);
+        showError(`Failed to get attribs/uniforms:\n`  +
+            `pos=${pos_attrib}\ncolor=${clr_attrib}\n` +
+            `u_model=${!!model_uniform}\nu_view=${!!view_uniform}\nu_proj=${!!proj_uniform}`);
         return;
     }
 
@@ -84,12 +44,12 @@ function main() {
 
     //
     // Create matrices
-    const UP_VECTOR = [0, 1, 0]
-    const model = mat4.create();
+    const UP_VECTOR = [0, 1, 0];
+    const CAM_POS = [0, 4, -4];
     const view = mat4.create();
     const proj = mat4.create();
 
-    mat4.lookAt(view, [4, 4, -4], [0, 0, 0], [0, 1, 0]);
+    mat4.lookAt(view, CAM_POS, [0, 0, 0], [0, 1, 0]);
     mat4.perspective(proj, glm.glMatrix.toRadian(45), WIDTH / HEIGHT, 0.1, 1000);
 
     setupRender(gl, canvas, WIDTH, HEIGHT, [0.75, 0.85, 0.8, 1.0]);
@@ -102,22 +62,22 @@ function main() {
     // Create entities
     const entities = [
         // init cube
-        new Entity([0, 0, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
+        new Entity([0, 0, 0], 1, UP_VECTOR, 0, cube.local_aabb, cube_vao, cube.indices.length),
 
-        // cubes along z axis
-        new Entity([0, 0, -2], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
-        new Entity([0, 0, 2], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
-        new Entity([0, 0, 4], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
+        /* // cubes along z axis
+        new Entity([0, 0, -2], 1, UP_VECTOR, 0, cube.local_aabb, cube_vao, cube.indices.length),
+        new Entity([0, 0, 2], 1, UP_VECTOR, 0,  cube.local_aabb, cube_vao, cube.indices.length),
+        new Entity([0, 0, 4], 1, UP_VECTOR, 0,  cube.local_aabb, cube_vao, cube.indices.length),
 
         // cubes along x axis
-        new Entity([-4, 0, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
-        new Entity([-2, 0, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
-        new Entity([2, 0, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
+        new Entity([-4, 0, 0], 1, UP_VECTOR, 0, cube.local_aabb, cube_vao, cube.indices.length),
+        new Entity([-2, 0, 0], 1, UP_VECTOR, 0, cube.local_aabb, cube_vao, cube.indices.length),
+        new Entity([2, 0, 0], 1, UP_VECTOR, 0,  cube.local_aabb, cube_vao, cube.indices.length),
 
         // cubes along y axis
-        new Entity([0, -2, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
-        new Entity([0, -4, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
-        new Entity([0, 2, 0], 1, UP_VECTOR, 0, cube_vao, cube.indices.length),
+        new Entity([0, -2, 0], 1, UP_VECTOR, 0, cube.local_aabb, cube_vao, cube.indices.length),
+        new Entity([0, -4, 0], 1, UP_VECTOR, 0, cube.local_aabb, cube_vao, cube.indices.length),
+        new Entity([0, 2, 0], 1, UP_VECTOR, 0,  cube.local_aabb, cube_vao, cube.indices.length), */
     ]
 
     const frame = function (should_loop) {
@@ -126,15 +86,51 @@ function main() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         entities.forEach(entity => entity.draw(gl, model_uniform));
-        /* entity.draw(gl, model_uniform) */
-        /* gl.bindVertexArray(cube_vao);
-        gl.drawElements(gl.TRIANGLES, cube.indices.length, gl.UNSIGNED_SHORT, 0); */
 
         if (should_loop) requestAnimationFrame(frame);
     }
 
+    const ray = {
+        origin: CAM_POS,
+    };
+
+    canvas.addEventListener("click", (e) => {
+        const rect = canvas.getBoundingClientRect();
+        const mouse_x = e.clientX - rect.left;
+        const mouse_y = e.clientY - rect.top;
+
+        // transform in reverse!
+        
+        const x_ndc = (2 * mouse_x) / WIDTH - 1;
+        const y_ndc = 1 - (2 * mouse_y) / HEIGHT;
+
+        
+        const ray_clip = [x_ndc, y_ndc, -1.0, 1.0];
+
+
+        const ray_eye = vec4.transformMat4([], ray_clip, mat4.invert([], proj));
+        ray_eye[2] = -1,
+        ray_eye[3] = 0;
+
+        let ray_world = vec4.transformMat4([], ray_clip, mat4.invert([], view));
+        ray_world = [ray_world[0], ray_world[1], ray_world[2]];
+        vec3.normalize(ray_world, ray_world);
+
+        console.log(ray_world);
+        ray.dir = ray_world;
+
+        entities.forEach(entity => {
+            if (entity.isIntersecting(ray)) {
+                console.log(entity);
+            }
+        });
+    });
+
+
+
     frame(false)
 }
+
 
 const WIDTH = 800;
 const HEIGHT = 600;
--- util/entity.js
+++ util/entity.js
@@ -1,4 +1,119 @@
+const glm = glMatrix; // shorten math library name,
+const vec3 = glm.vec3;
+const mat4 = glm.mat4;
 import { showError } from "./gl_utils.js";
+
+export const colors = 
+[
+[0.2, 0.2, 0.2], [0.8, 0.2, 0.2],
+[0.2, 0.8, 0.2], [0.2, 0.2, 0.8],
+[0.2, 0.8, 0.8], [0.8, 0.2, 0.8],
+[0.8, 0.8, 0.2], [0.8, 0.8, 0.8]
+]
+
+export class Entity {
+    constructor(pos, scale, r_axis, r_angle, local_aabb, vao, num_indices) {
+        this.vao = vao;
+        this.num_indices = num_indices;
+
+        this.local_aabb = local_aabb;
+
+        // build model matrix
+        this.mat_model = mat4.create();
+        mat4.translate(this.mat_model, this.mat_model, pos);
+        mat4.rotate(this.mat_model, this.mat_model, glm.glMatrix.toRadian(r_angle), r_axis);
+        mat4.scale(this.mat_model, this.mat_model, [scale, scale, scale]);
+
+        this.computeWorldAABB();
+    }
+
+    computeWorldAABB() {
+        let world_x = [];
+        let world_y = [];
+        let world_z = [];
+
+        // first interpolate all 8 corners of local AABB
+        let  local_corners = [
+            this.local_aabb.max[0], this.local_aabb.max[1], this.local_aabb.max[2], // front-top-right
+            this.local_aabb.max[0], this.local_aabb.min[1], this.local_aabb.max[2], // front-bot-right
+            this.local_aabb.min[0], this.local_aabb.max[1], this.local_aabb.max[2], // front-top-left
+            this.local_aabb.min[0], this.local_aabb.min[1], this.local_aabb.max[2], // front-bot-left
+
+            this.local_aabb.max[0], this.local_aabb.max[1], this.local_aabb.min[2], // back-top-right
+            this.local_aabb.max[0], this.local_aabb.min[1], this.local_aabb.min[2], // back-bot-right
+            this.local_aabb.min[0], this.local_aabb.max[1], this.local_aabb.min[2], // back-top-left
+            this.local_aabb.min[0], this.local_aabb.min[1], this.local_aabb.min[2], // back-bot-left
+        ];
+
+        // transform 8 corners to world space
+        for (let i = 0; i < local_corners.length; i+=3) {
+            let world_point = vec3.transformMat4([], 
+                [local_corners[i], local_corners[i+1], local_corners[i+2]], this.mat_model);
+            world_x.push(world_point[0]);
+            world_y.push(world_point[1]);
+            world_z.push(world_point[2]);
+        }
+
+        // find the min and max of the new corners
+        const max_x = Math.max(...world_x);
+        const min_x = Math.min(...world_x);
+        const max_y = Math.max(...world_y);
+        const min_y = Math.min(...world_y);
+        const max_z = Math.min(...world_z);
+        const min_z = Math.min(...world_z);
+
+        this.world_aabb = {
+            max: [max_x, max_y, max_z],
+            min: [min_x, min_y, min_z]
+        };
+        console.log(this.world_aabb);
+    }
+
+    isIntersecting(ray) {
+        let tmin = (this.world_aabb.min[0] - ray.origin[0]) / ray.dir[0];
+        let tmax = (this.world_aabb.max[0] - ray.origin[0]) / ray.dir[0];
+
+        if (tmin > tmax) {
+            let temp = tmax;
+            tmax = tmin;
+            tmin = temp;
+        }
+
+        let tymin = (this.world_aabb.min[1] - ray.origin[1]) / ray.dir[1];
+        let tymax = (this.world_aabb.max[1] - ray.origin[1]) / ray.dir[1];
+
+        if (tymin > tymax) {
+            let temp = tymax;
+            tymax = tymin;
+            tymin = temp;
+        }
+
+        if ((tmin > tymax) || (tymin > tmax)) return false;
+
+        if (tymin > tmin) tmin = tymin; 
+        if (tymax < tmax) tmax = tymax;
+
+        let tzmin = (this.world_aabb.min[2] - ray.origin[2]) / ray.dir[2];
+        let tzmax = (this.world_aabb.min[2] - ray.origin[2]) / ray.dir[2];
+
+        if (tzmin > tzmax) {
+            let temp = tzmax;
+            tzmax = tzmin;
+            tzmin = temp;
+        }
+
+        if ((tmin > tzmax) || (tzmax > tmax)) return false;
+
+        return true;
+    }
+
+    draw(gl, model_uniform) {
+        gl.uniformMatrix4fv(model_uniform, false, this.mat_model);
+        gl.bindVertexArray(this.vao);
+        gl.drawElements(gl.TRIANGLES, this.num_indices, gl.UNSIGNED_SHORT, 0);
+        gl.bindVertexArray(null);
+    }
+}
 
 export function createCubeMultiColored(colors) {
 
@@ -35,7 +150,15 @@ export function createCubeMultiColored(colors) {
         1, 5, 7, // bottom
         1, 3, 7
     ];
-    return {vertices, indices};
+
+    // know that the bounding box coordinates are: top
+
+    const local_aabb = {
+        max: [ 0.5,  0.5,  0.5],
+        min: [-0.5, -0.5, -0.5]
+    };
+
+    return {vertices, indices, local_aabb};
 }
 
 export function createPosClrInterleavedVao(gl, vertex_buffer, index_buffer, pos_attrib, clr_attrib) {
